ece2fab083b35a792d89e808fe5f8b5ee548dc61
manoflogan <manoflogan@gmail.com>
2019-09-02 00:07:17 -0700

    Inserts item to the list.

--- superstack.py
+++ superstack.py
@@ -6,17 +6,17 @@ class SuperStack(object):
     def __init__(self):
         self.stack = list()
 
-    def push(self, k: int) -> int:
+    def push(self, k: int) -> None:
         """Inserts entry to the list."""
         self.stack.append(k)
 
-    def pop(self) -> None:
+    def pop(self) -> int:
         """Pops the top element from the list"""
         return self.stack.pop()
 
     def inc(self, e: int, k: int) -> None:
-        """Adds e to the bottom k elements of the stack."""
-        if len(self.stack) < k:
+        """Adds k to the bottom e elements of the stack."""
+        if len(self.stack) < e:
             return
 
         for index in range(0, e):
